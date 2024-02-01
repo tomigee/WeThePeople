b05a9ac363beaa91657b51c61e755816ffda22d4
tomigee <8400294+tomigee@users.noreply.github.com>
2024-01-31 22:51:46 -0700

    Re-architected the package, given Congress, Fred, and BEA. Removed all private information, in order to finally publicize the repo.

---
+++ datafetcher/script.py
@@ -0,0 +1,3 @@
+# In this file, I hope to compile:
+# 1. All passed legislature in the United States since time immemorial
+# 2. Time series of the 10 most popular economic indicators from FRED
--- datafetcher/utils.py
+++ datafetcher/utils.py
@@ -1,2 +1,27 @@
+from io import StringIO
+from html.parser import HTMLParser
+
+
 def format_source(source):
     return source.lower()
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, d):
+        self.text.write(d)
+
+    def get_data(self):
+        return self.text.getvalue()
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
